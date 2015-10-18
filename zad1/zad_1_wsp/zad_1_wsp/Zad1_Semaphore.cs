71ab1bd4a10687d3bb3c9cb3b05dc48867761571
Daria Woznicka <woznicka.daria@gmail.com>
2015-10-18 20:52:10 +0200

    initial commit

---
+++ zad1/zad_1_wsp/zad_1_wsp/Zad1_Semaphore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace zad_1_wsp
+{
+	public class Zad1_Semaphore
+	{
+		static Thread[] threads = new Thread[2];
+		static Semaphore sem = new Semaphore(1, 1);
+		public Zad1_Semaphore ()
+		{
+		}
+		static void semaphoring()
+		{
+			Console.WriteLine("{0} is waiting in line...", Thread.CurrentThread.Name);
+			sem.WaitOne();
+			Console.WriteLine("{0} enters the semaphore!", Thread.CurrentThread.Name);
+			Thread.Sleep(300);
+			Console.WriteLine("{0} is leaving the semaphore", Thread.CurrentThread.Name);
+			sem.Release();
+		}
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 2; i++)
+			{
+				threads[i] = new Thread(semaphoring);
+				threads[i].Name = "thread_" + i;
+				threads[i].Start();
+			}
+			Console.Read();
+		}
+	}
+}
