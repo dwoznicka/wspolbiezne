6651e70cb141b46ec24d4a8e1014fa18e260db4a
wisniaCFC <adam.wisniewski93@gmail.com>
2015-10-18 23:51:28 +0200

    Działający program :)

---
+++ Semafory/Semafory/Logic.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semafory
+{
+    public class Logic
+    {
+        public int temp = -1;
+        public int indexY = -1;
+        public int indexX = -1;
+        public List<int> x = new List<int>();
+        public List<int> y = new List<int>();
+        public bool stop = false;
+
+        public void InsertSize()
+        {
+            Console.WriteLine("Wpisz rozmiar ciągu x: ");
+            var tempSizeX = Console.ReadLine();
+            int sizeX = int.Parse(tempSizeX.ToString());
+
+            Console.WriteLine("Wpisz rozmiar ciągu y: ");
+            var tempSizeY = Console.ReadLine();
+            int sizeY = int.Parse(tempSizeY.ToString());
+            Random rand = new Random();
+
+
+            for (int i = 0; i < sizeX; i++)
+            {
+                int temp = rand.Next(1, 100);
+                x.Add(temp);
+            }
+
+            for (int i = 0; i < sizeY; i++)
+            {
+                int temp = rand.Next(1, 100);
+                y.Add(temp);
+            }
+        }
+
+        public void ShowLists()
+        {
+            Console.WriteLine("Czy chcesz wyświetlić listy? (y/n)");
+
+            string input = Console.ReadLine();
+            if (input == "y" || input == "yes" || input == "Y")
+            {
+                Console.WriteLine("Ciąg x:");
+                foreach (int val in x)
+                {
+                    Console.WriteLine(val);
+                }
+
+                Console.WriteLine("");
+
+                Console.WriteLine("Ciąg y:");
+
+                foreach (int val in y)
+                {
+                    Console.WriteLine(val);
+                }
+            }
+        }
+
+        public int FindOddNumberInY()
+        {
+            for (int i = 0; i < y.Count; i++)
+            {
+                if (y[i] % 2 != 0)
+                {
+                    if (indexX == -1 && temp == -1)
+                    {
+                        indexY = i;
+                        return y[i];
+                    }
+                    else if(indexX != -1 && temp != -1)
+                    {
+                        SwapFromYtoX(y[i], i);
+                    }
+                }
+            }
+            stop = true;
+            return -1;
+        }
+
+        public int FindEvenNumberInX()
+        {
+            for (int i = 0; i < x.Count; i++)
+            {
+                if (x[i] % 2 == 0)
+                {
+                    if (indexY == -1 && temp == -1)
+                    {
+                        indexX = i;
+                        return x[i];
+                    }
+                    else if(indexY != -1 && temp != -1)
+                    {
+                        SwapFromXtoY(x[i], i);
+                    }
+                }
+            }
+            stop = true;
+
+            return -1;
+        }
+
+        public void SwapFromYtoX(int currentY, int currentYindex)
+        {
+            Console.WriteLine("Swapping y[" + currentYindex + "] value (" + currentY + ") with " + x[indexX]);
+
+            x[indexX] = currentY;
+            y[currentYindex] = temp;
+            temp = -1;
+            indexX = -1;
+        }
+
+        public void SwapFromXtoY(int currentX, int currentXindex)
+        {
+            Console.WriteLine("Swapping x[" + currentXindex + "] value (" + currentX + ") with " + y[indexY]);
+
+            y[indexY] = currentX;
+            x[currentXindex] = temp;
+            temp = -1;
+            indexY = -1;
+        }
+    }
+}
--- Semafory/Semafory/Program.cs
+++ Semafory/Semafory/Program.cs
@@ -7,27 +7,53 @@ using System.Threading.Tasks;
 
 namespace Semafory
 {
-    class Program
+    public class Program
     {
-        static Thread[] threads = new Thread[2];
+        public Logic log = new Logic();
+
         static Semaphore sem = new Semaphore(1, 1);
-        static void semaphoring()
+
+        void semaphoring()
         {
-            Console.WriteLine("{0} is waiting in line...", Thread.CurrentThread.Name);
-            sem.WaitOne();
-            Console.WriteLine("{0} enters the semaphore!", Thread.CurrentThread.Name);
-            Thread.Sleep(300);
-            Console.WriteLine("{0} is leaving the semaphore", Thread.CurrentThread.Name);
-            sem.Release();
+            while (log.stop == false)
+            {
+                Console.WriteLine("{0} is waiting in line...", Thread.CurrentThread.Name);
+                sem.WaitOne();
+                Console.WriteLine("{0} enters the semaphore!", Thread.CurrentThread.Name);
+                if (Thread.CurrentThread.Name == "thread_x")
+                {
+                    if (log.indexX == -1)
+                        log.temp = log.FindEvenNumberInX();
+                }
+                else if(Thread.CurrentThread.Name == "thread_y")
+                {
+                    if (log.indexY == -1)
+                        log.temp = log.FindOddNumberInY();
+                }
+                Thread.Sleep(300);
+                Console.WriteLine("{0} is leaving the semaphore", Thread.CurrentThread.Name);
+                sem.Release();
+            }
         }
+
+
         static void Main(string[] args)
         {
-            for (int i = 0; i < 2; i++)
-            {
-                threads[i] = new Thread(semaphoring);
-                threads[i].Name = "thread_" + i;
-                threads[i].Start();
-            }
+            Program pro = new Program();
+
+            pro.log.InsertSize();
+            pro.log.ShowLists();
+            
+            Thread threadX = new Thread(pro.semaphoring);
+            threadX.Name = "thread_x";
+            threadX.Start();
+
+            Thread threadY = new Thread(pro.semaphoring);
+            threadY.Name = "thread_y";
+            threadY.Start();
+
+            pro.log.ShowLists();
+
             Console.Read();
         }
     }
